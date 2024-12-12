78e851371172ed9a21bb3475e0b36b7138e5d6f9
Calvin Min <Calvinjmin@gmail.com>
2024-12-12 16:38:37 -0500

    Day 12 Part 2 2024

--- 2024/12/main.cpp
+++ 2024/12/main.cpp
@@ -1,4 +1,5 @@
 #include "../utils.h"
+#include <queue>
 #include <iostream>
 #include <set>
 using namespace std;
@@ -40,7 +41,7 @@ int bfs(int x, int y, char symbol) {
     }
   }
 
-  cout << "Region Area: " << region.size() << endl;
+  cout << "Region Area: " << region.size() << symbol << endl;
   int permi = 0;
   for (auto const &[a, b] : region) {
     for (int i = 0; i < 4; i++) {
@@ -80,36 +81,81 @@ int bfs2(int x, int y, char symbol) {
   }
 
   cout << "Region Area: " << region.size() << endl;
-  set<pair<int, int>> distinctSides;
-  for (auto const &[a, b] : region) {
-    for (int i = 0; i < 4; i++) {
-      int newX = a + dir[i].first;
-      int newY = b + dir[i].second;
-      if (outsideGrid(newX, newY)) {
-        // check up or down
-        if ( i == 0 || i == 1 ) {
-            distinctSides.insert({b,i});
-        }
-        else {
-            distinctSides.insert({a,i});
-        }
-      } else if (validPoint(newX, newY) && grid[newX][newY] != symbol) {
-        // check up or down
-        if ( i == 0 || i == 1 ) {
-            distinctSides.insert({b,i});
-        }
-        else {
-            distinctSides.insert({a,i});
+
+  // Side Calculation
+  set<tuple<int, int,int>> distinctSides;
+  int sides = 0;
+  for ( int i = 0; i < 4; i++ ) {
+    queue<tuple<int,int,int>> q;
+    for ( auto const &[a,b] : region ) {
+        int newX = a + dir[i].first;
+        int newY = b + dir[i].second;
+        
+        // Case for a distinct side
+        if ( distinctSides.find({a,b,i}) == distinctSides.end() && ( outsideGrid(newX, newY) || ( validPoint(newX, newY) && grid[newX][newY] != symbol ) ) ) {
+          cout << a << " " << b << " " << i << " " << grid[a][b] << endl;
+          sides += 1;
+          q.push( {a,b,i} );
+
+        // Continuously motion on a distinct side
+        while( !q.empty() ) {
+          auto point = q.front();
+          q.pop();
+          if ( distinctSides.find(point) != distinctSides.end() ) {
+            continue;
+          } else {
+            distinctSides.insert(point);
+          }
+
+          // Direction is up or down
+          if ( i == 2 || i == 3 ) {
+            int x = get<0>(point);
+            int y = get<1>(point);
+            int direction = get<2>(point);
+            int left = y - 1;
+            int right = y + 1;
+
+            int looking = x + dir[i].first;
+
+            // Make sure the left or right block is the same symbol and the dir we are looking is an edge
+            if ( validPoint(x, left) && grid[x][left] == symbol ) {
+              if ( outsideGrid(looking, left) || (validPoint(looking,left) && grid[looking][left] != symbol) ) {
+                q.push( {x, left, direction} );
+              }
+            }
+            if ( validPoint(x, right) && grid[x][right] == symbol ) {
+              if ( outsideGrid(looking, right) || (validPoint(looking,right) && grid[looking][right] != symbol) ) {
+                q.push( {x, right, direction} );
+              }
+            }
+            
+          } else { // Direction is left or right
+            int x = get<0>(point);
+            int y = get<1>(point);
+            int direction = get<2>(point);
+            int up = x - 1;
+            int down = x + 1;
+
+            int looking = y + dir[i].second;
+
+            // Make sure the up or down block is the same symbol and the dir we are looking is an edge
+            if ( validPoint(up, y) && grid[up][y] == symbol ) {
+              if ( outsideGrid(up, looking) || (validPoint(up,looking) && grid[up][looking] != symbol) ) {
+                q.push( {up, y, direction} );
+              }
+            }
+            if ( validPoint(down, y) && grid[down][y] == symbol ) {
+              if ( outsideGrid(down, looking) || (validPoint(down,looking) && grid[down][looking] != symbol) ) {
+                q.push( {down, y, direction} );
+              }
+            }
+
+          }
         }
       }
     }
   }
-
-    for ( auto pair : distinctSides ) {
-        cout << pair.first << " " << pair.second << endl;
-    }
-    cout << "Sides: " << distinctSides.size() << endl;
-  return region.size() * distinctSides.size();
+  return region.size() * sides; 
 }
 
 void part1() {
@@ -168,3 +214,4 @@ int main() {
   // part1();
   part2();
 }
+
