9c539f38cfb26d9edc757d174b331ebf9496e228
Calvin Min <calvin.min@databricks.com>
2025-12-11 19:51:14 -0800

    2025 - Day 11: Part 2 Cache Impl

--- 2025/11/main.cpp
+++ 2025/11/main.cpp
@@ -12,6 +12,7 @@ const string& endNode = "out";
 
 const string& startNodeP2 = "svr";
 map<string, vector<string>> edges;
+map<tuple<string,bool,bool>, unsigned long long> memo;
 
 void dfs( int& recur, const string& key ) {
     if ( key == endNode ) {
@@ -23,26 +24,23 @@ void dfs( int& recur, const string& key ) {
     }
 }
 
-// tldr; too inefficient .... maybe there is an opp for early pruning
-void dfs2( int& recur, const string& key, unordered_set<string>& path, bool hasDac, bool hasFft ) {
+unsigned long long dfs2( const string& key, bool hasDac, bool hasFft ) {
     if ( key == "dac" ) hasDac = true;
     if ( key == "fft" ) hasFft = true;
 
     if ( key == endNode ) {
-        if ( hasDac && hasFft ) {
-            recur += 1;
-        }
-        return;
+        return (hasDac && hasFft) ? 1 : 0;
     }
 
-    path.insert(key);
-    const vector<string>& currEdge = edges[key];
-    for ( const string& node: currEdge ) {
-        if ( path.count(node) == 0 ) {
-            dfs2(recur, node, path, hasDac, hasFft);
-        }
+    unsigned long long total = 0;
+
+    // assumption - no cycle, use memo to capture state and return totals AKA cache
+    auto state = make_tuple(key, hasDac, hasFft);
+    if ( memo.count(state) ) return memo[state];
+    for ( const string& node: edges[key] ) {
+        total += dfs2(node, hasDac, hasFft);
     }
-    path.erase(key);
+    return memo[state] = total;
 }
 
 void part1() {
@@ -81,9 +79,9 @@ void part2() {
         }
     }
 
-    int recur = 0;
+    unsigned long long recur = 0;
     unordered_set<string> path;
-    dfs2( recur, startNodeP2, path, false, false);
+    recur = dfs2( startNodeP2, false, false);
     cout << "Answer: " << recur << endl;
 }
 
